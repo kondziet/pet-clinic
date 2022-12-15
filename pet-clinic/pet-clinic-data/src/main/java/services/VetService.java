df3a533b477f8918a7a3a07dad5a6f9cab8c3182
Konrad Dabrowski <konrad.dabrowski54@gmail.com>
2022-12-15 18:34:01 +0100

    Add interfaces for services

---
+++ pet-clinic/pet-clinic-data/src/main/java/services/VetService.java
@@ -0,0 +1,14 @@
+package services;
+
+import pl.kondziet.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
